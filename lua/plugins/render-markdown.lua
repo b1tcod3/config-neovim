0286e03d7145583651f941661619c3055810b206
Jose Elias Romero Guanipa <joseromerogc@gmail.com>
2026-07-14 13:09:00 -0400

    fix: instalar parsers markdown via TSInstall en build

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,7 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown" },
+    build = ":TSInstall markdown markdown_inline",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     opts = {},
 }
